7fc6ae8d2b36ac180d7d44ebb898aefc01f352ce
naianebatista <nbatista@ciandt.com>
2022-08-10 11:09:07 -0300

    Métodos feedback service implementados

--- src/main/java/com/ciandt/feedfront/services/FeedbackServiceImpl.java
+++ src/main/java/com/ciandt/feedfront/services/FeedbackServiceImpl.java
@@ -8,6 +8,7 @@ import com.ciandt.feedfront.repositories.FeedbackRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Optional;
 
 //TODO: IMPLEMENTE AS CLASSES E MAPEIE A CLASSE PARA O SPRINGBOOT
 
@@ -17,18 +18,20 @@ public class FeedbackServiceImpl implements FeedbackService {
     private FeedbackRepository feedBackRepository;
 
     @Override
-    public List<Feedback> listar() {
-        throw new UnsupportedOperationException();
+    public List<Feedback> listar() throws  UnsupportedOperationException{
+        return feedBackRepository.findAll();
+
     }
 
     @Override
-    public Feedback buscar(long id) throws BusinessException {
-        throw new UnsupportedOperationException();
+    public Feedback buscar(long id) throws BusinessException,UnsupportedOperationException {
+        Optional<Feedback>feedback=feedBackRepository.findById(id);
+        return feedback.get();
     }
 
     @Override
-    public Feedback salvar(Feedback feedback) throws BusinessException, IllegalArgumentException {
-        throw new UnsupportedOperationException();
+    public Feedback salvar(Feedback feedback) throws BusinessException, IllegalArgumentException,UnsupportedOperationException {
+        return feedBackRepository.save(feedback);
     }
 
 }
